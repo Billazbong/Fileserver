6937dfe3bcf94679665aba3d4575f26eb4c63609
billabong <billabong@dsqds.myguest.virtualbox.org>
2024-11-05 16:44:59 +0100

    Updated server.c to add basic initialization process such as creating the storage directory

--- server/server.c
+++ server/server.c
@@ -5,29 +5,62 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/stat.h>
 #include <sys/socket.h>
 #include <sys/select.h>
 #include <fcntl.h>
 #include <netinet/in.h>
+#include <errno.h>
 
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+#define err(s,n) {perror("In file "__FILE__" at line "TOSTRING(__LINE__)": "#s);exit(n);}
+
+#define STORAGE_DIR "/var/lib/myfileserver/"
 #define MAX_CLIENT 10
 
 typedef struct 
 {
-    int server_sock; // Server socket
-    int client_socks[MAX_CLIENT]; // Array of the client sockets
-    fd_set client_fds; // File descriptor set for select()
+    int server_fd; // Server socket
+    int client_fds[MAX_CLIENT]; // Array of the client sockets
+    fd_set client_fdset; // File descriptor set for select()
     int max_fd; // Max file descriptor for select()
     struct sockaddr_in serv_addr;
 }Server;
 
+/*Function to setup the storage directory if it does not exists*/
 
-void init(Server server){
+void setup_stordir(){
+    struct stat st={0};
+    if (stat(STORAGE_DIR,&st)==-1) {
+        if (mkdir(STORAGE_DIR,0700)==-1) err(Could not setup the storage directory,5)
+        else printf("Storage directory created at : %s\n", STORAGE_DIR);
+    }
+    else printf("Storage directory already exists at : %s\n",STORAGE_DIR);
+}
+
+
+/*Function to initialize the server*/
+
+void init(Server *server,int port){
     
+    printf("Setting up the storage directory...\n");
+    setup_stordir();
+
+    memset(server,0,sizeof(Server));
+    server->server_fd=socket(AF_INET, SOCK_STREAM, 0);
+
+    if (server->server_fd<0) err(Could not initialize the server socket,4);
+    server->max_fd=server->server_fd;
 }
 
 
 int main(int argc, char **argv){
+    if (argc !=2) err(Wrong number of argument,2);
+    int port;
+    if ((port=atoi(argv[1]))==0) err(Port have to be a number or different from 0,3);
     Server server;
-    init(server);
+    printf("Initializing server...\n");
+    init(&server,port);
+    return 43;
 }
